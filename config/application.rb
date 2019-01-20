de561cac088e27bebf9cf7f445fc606120942fa3
Conrad Taylor <conradwt@gmail.com>
2019-01-19 20:31:52 -0800

    Set the default generators.

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,20 @@ module Curriculum
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+     # configure file generation for both application and specifications.
+    config.generators do |g|
+      g.helper          false
+      g.orm             :active_record
+      g.template_engine :erb
+      g.test_framework  :rspec,
+                        controller_specs: false,
+                        fixture: false,
+                        helper_specs: false,
+                        routing_specs: false,
+                        view_specs: false
+      g.stylesheets     false
+      g.javascripts     false
+    end
   end
 end
