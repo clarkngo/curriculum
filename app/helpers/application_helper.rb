a1697711d1de011008f9e0df14a4f59cbda91972
Conrad Taylor <conradwt@gmail.com>
2019-01-22 23:36:59 -0800

    Reset application layout and helper files.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,23 @@
 module ApplicationHelper
+  def bootstrap_class_for( flash_type )
+    case flash_type
+      when 'success', 'notice'
+        'alert-success'
+      when 'error', 'alert'
+        'alert-danger'
+      else
+        flash_type.to_s
+    end
+  end
+
+  def bootstrap_string_for( flash_type )
+    case flash_type
+      when "success", "notice"
+        "Success"
+      when "error", "alert"
+        "Error!"
+      else
+        flash_type.to_s
+    end
+  end
 end
