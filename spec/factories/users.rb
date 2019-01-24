0fad9f1e46dee69a416eb750add176a91bed72ef
Conrad Taylor <conradwt@gmail.com>
2019-01-23 21:51:20 -0800

    Remove unecessary field, username.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,5 @@
 FactoryBot.define do
   factory :user do
-    sequence(:username) { |n| "jdoe#{n}" }
     sequence(:email) { |n| "jdoe#{n}@example.com" }
     password { "password" }
     password_confirmation { "password" }
