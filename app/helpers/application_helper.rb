145244211af13c32ec7b45676021e0d240a6f6d9
Clark Ngo <clarkngo@gmail.com>
2019-01-24 20:32:26 -0800

    flash msg fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -20,4 +20,13 @@ module ApplicationHelper
         flash_type.to_s
     end
   end
+
+  def flash_class(level)
+    case level
+        when 'notice' then "alert alert-dismissable alert-info"
+        when 'success' then "alert alert-dismissable alert-success"
+        when 'error' then "alert alert-dismissable alert-danger"
+        when 'alert' then "alert alert-dismissable alert-danger"
+    end
+  end
 end
