665f908482e26c818821da303b9fa225a5aa06d3
Clark Ngo <clarkngo@gmail.com>
2019-02-02 05:45:25 -0800

    model test for confirmation email

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+describe "after creation" do
+  it "sends a confirmation email" do
+    user = FactoryBot.create(:user)
+
+    expect { user.save }.to change(
+        Devise.mailer.deliveries, :count
+    ).by(1)
+  end
+ end
+ 
