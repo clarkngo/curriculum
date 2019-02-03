2b3169efa9949eccd247158f9badd03f2feba5e4
Clark Ngo <clarkngo@gmail.com>
2019-02-02 17:15:21 -0800

    configured factory bot for create course

--- spec/factories/courses.rb
+++ spec/factories/courses.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :course do
-    
+    category { "Preparatory"}
+    name { "Intro to Computer Science" }
+    description { "This course is an introduction to Computer Science" }    
   end
 end
