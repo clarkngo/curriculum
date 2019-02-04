01f98fa7e427295f8391915252df44eff8bfcd4d
Clark Ngo <clarkngo@gmail.com>
2019-02-03 18:02:09 -0800

    test refactored

--- spec/features/user_sign_up_confirmation_spec.rb
+++ spec/features/user_sign_up_confirmation_spec.rb
@@ -3,12 +3,14 @@ require 'rails_helper'
 RSpec.feature "user signup process confirmation", type: :feature do
   scenario 'user signup' do
     # arrange
-    visit "/users/sign_up"
+    @user = FactoryBot.build(:user)
+    visit root_path
+
+    click_link "Sign up"
     
-    fill_in 'Email', with: 'jdoe@example.com'
-    # cannot use Password because it appears in Password Confirmation
-    fill_in 'user_password', with: 'password'
-    fill_in 'Password confirmation', with: 'password'
+    fill_in "Email", :with => @user.email
+    fill_in "Password", :with => @user.password
+    fill_in "Password confirmation", :with => @user.password
 
     # act
     click_button 'Sign up'
