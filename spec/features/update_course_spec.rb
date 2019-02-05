44ff2ac65ad6362a5e826a756d2430f9975471ea
Clark Ngo <clarkngo@gmail.com>
2019-02-05 00:22:21 -0800

    test for update course

--- spec/features/update_course_spec.rb
+++ spec/features/update_course_spec.rb
@@ -4,30 +4,17 @@ RSpec.feature "update course", :type => :feature do
   scenario "User updates a course" do
 
     ## User sign in
-
-    # arrange
-    user = FactoryBot.create(:user)
-
     visit '/users/sign_in'
     
-    fill_in 'Email', with: user.email
-    fill_in 'Password', with: user.password
+    fill_in 'Email', :with => "someone@example.com"
+    fill_in 'Password', :with => "password"
 
-    # assert
-    expect(page).to have_xpath('/')
-
-
-
-    # act
     click_on 'Log in'
 
-    click_on 'Suggest New Course'
-    # act
+    ## Update course
+    visit '/courses'
 
-    course = FactoryBot.create(:course)
-
-    visit '/courses/'
-    ## Updating a course 
+    first('.edit').click_link('Edit')
     
     fill_in "Name", :with => "Intro to C++"
     fill_in "Description", :with => "This course is an intro to C++."
