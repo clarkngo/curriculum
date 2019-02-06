217faf222743190814949fcce10c6953bc298a2f
Clark Ngo <clarkngo@gmail.com>
2019-02-06 12:46:04 -0800

    temporary factorybot configuration

--- spec/factories/courses.rb
+++ spec/factories/courses.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :course do
-    
+    category {FactoryBot.create(:category)}
+    name {'Intro to Machine Learning'}
+    description {'This course is an intro to machine learning.'}
+    user {FactoryBot.create(:user)}
   end
 end
