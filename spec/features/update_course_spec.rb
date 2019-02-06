b212ef894a59ca42215916ae827d82c1d3941986
Clark Ngo <clarkngo@gmail.com>
2019-02-05 21:51:42 -0800

    not working test

--- spec/features/update_course_spec.rb
+++ spec/features/update_course_spec.rb
@@ -3,20 +3,20 @@ require "rails_helper"
 RSpec.feature "update course", :type => :feature do
   scenario "User updates a course" do
 
-    ## User sign in
+    course = FactoryBot.create(:course)
+    
+    ## User sign in 
     visit '/users/sign_in'
     
-    fill_in 'Email', :with => "someone@example.com"
+    fill_in 'Email', :with => course.user.email
     fill_in 'Password', :with => "password"
 
     click_on 'Log in'
-
     ## Update course
     visit '/courses/1/edit'
     
-    select "Preparatory", from: 'Category'
-    fill_in "Name", :with => "Intro to C++"
-    fill_in "Description", :with => "This course is an intro to C++."
+    fill_in("Name", with: "Intro to C++")
+    fill_in("Description", with: "This course is an intro to C++.")
     click_button "Submit!"
 
     expect(page).to have_text("Course was succesfully updated.")
