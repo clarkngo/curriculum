bbb935496142c1ca50c3fac0e58ba65fc413f73c
Clark Ngo <30854108+clarkngo@users.noreply.github.com>
2019-02-06 21:09:31 -0800

    Delete course (#61)
    * added destroy button on course view
    
    * added destroy action
    
    * refactor code for destroy function
    
    * factorybot for categories
    
    * add feature spec
    
    * added flash message for course deletion
    
    * rename file for feature test for destroy action
    
    * rename flash message ..destroyed.. to ..deleted..
    
    * rename link Destroy to Delete

---
+++ spec/features/user_deletes_course_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.feature "user deletes a course process", type: :feature do
+  scenario 'user deletes a course' do
+
+    # arrange
+    user = FactoryBot.create(:user)
+    category = FactoryBot.create(:category)
+    course = Course.create(
+      category: category,
+      name: 'Intro to AI',
+      description: 'This course is an intro to AI.',
+      user: user
+    )
+
+    visit '/'
+    click_on 'Sign in'
+
+    fill_in 'Email', :with => user.email
+    fill_in 'Password', :with => user.password
+
+    click_on 'Log in'
+
+    visit course_path(course)
+
+    # act    
+    click_on 'Delete'
+
+    # assert
+    expect(page).to have_text("Successfully deleted a course!")
+  end
+end
