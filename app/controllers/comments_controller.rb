13e7f386c7c9adcb3e3bceeea6f0b70fddcfc699
Clark Ngo <clarkngo@gmail.com>
2019-02-08 00:28:05 -0800

    add create action for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,16 @@
 class CommentsController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    @course = Course.find(params[:course_id])
+    @course.comments.create(comment_params.merge(user: current_user))
+    flash[:success] = "Successfully added a comment!"
+    redirect_to course_path(@course)
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:message)
+  end
 end
