79be0fc2829938309386950f59310b4e1a195137
Clark Ngo <clarkngo@gmail.com>
2019-02-09 12:57:11 -0800

    add permit for custom column avatar in devise sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def render_not_found(status=:not_found)
     render plain: "#{status.to_s.titleize} :(", status: status
   end  
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:avatar])
+  end
 end
