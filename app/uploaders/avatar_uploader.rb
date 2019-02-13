2be1b673f86918e7af7eaf94dba4379143b63697
Clark Ngo <clarkngo@gmail.com>
2019-02-12 22:39:47 -0800

    add thumb versions for uploaded image

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -29,9 +29,9 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # end
 
   # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process resize_to_fit: [50, 50]
-  # end
+  version :thumb do
+    process resize_to_fit: [50, 50]
+  end
 
   process resize_to_fill: [180, 180]
 
