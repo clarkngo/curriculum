e7ecd2216b383b8f84b10d160a183469116aa49d
Clark Ngo <clarkngo@gmail.com>
2019-02-13 13:47:27 -0800

    remove sign up avatar upload in controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,6 @@ class ApplicationController < ActionController::Base
   end  
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:avatar])
     devise_parameter_sanitizer.permit(:account_update, keys: [:avatar])
   end
 end
