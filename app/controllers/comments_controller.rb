9e65e10e0f6dc13f2c7471b007fdca785290e597
Clark Ngo <clarkngo@gmail.com>
2019-02-16 01:51:30 -0800

    add edit, update and destroy actions for controller comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -14,6 +14,39 @@ class CommentsController < ApplicationController
     redirect_to course_path(@course)
   end
 
+  def edit
+    @comment = Comment.find_by_id(params[:id])
+    @course = @comment.course
+    return render_not_found if @comment.blank?
+    return render_not_found(:forbidden) if @comment.user != current_user
+  end
+    
+  def update
+    @comment = Comment.find_by_id(params[:id])
+    @course = @comment.course
+    return render_not_found if @comment.blank?
+    return render_not_found(:forbidden) if @comment.user != current_user
+    @comment.update_attributes(comment_params)
+    
+    if @comment.valid?
+      flash[:success] = "Comment was succesfully updated."
+      redirect_to dashboard_index_path
+    else
+      return render :edit, status: :unprocessable_entity
+    end
+  end
+    
+  def destroy
+    @comment = Comment.find(params[:id])
+    if @comment.user != current_user
+      return render plain: 'Not Allowed', status: :forbidden
+    end
+
+    @comment.destroy
+    flash[:success] = "Successfully deleted a comment!"
+    redirect_to dashboard_index_path
+  end
+
   private
 
   def comment_params
