a5e4843cdd233ba944ddeb2809ef16e41f094d3e
Conrad Taylor <conradwt@gmail.com>
2019-02-16 04:46:02 -0800

    Fix master branch (#70)
    * Fix CarrierWave setup and exclude uploads folder.
    
    * Fix dropdown menu and reposition course suggestion button.

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,24 @@
-# config/initializers/carrierwave.rb
+if Rails.env.development?
+  CarrierWave.configure do |config|
+    config.storage = :file
+  end
+end
 
-CarrierWave.configure do |config|
-  config.fog_provider = 'fog/aws'                        # required
-  config.fog_credentials = {
-    provider:              'AWS',                        # required
-    aws_access_key_id:     ENV["AWS_ACCESS_KEY"],        # required
-    aws_secret_access_key: ENV["AWS_SECRET_KEY"],        # required
-  }
-  config.fog_directory  = ENV["AWS_BUCKET"]              # required
+if Rails.env.test?
+  CarrierWave.configure do |config|
+    config.storage = :file
+    config.enable_processing = false
+  end
+end
+
+if Rails.env.productioon?
+  CarrierWave.configure do |config|
+    config.fog_provider = 'fog/aws'                        # required
+    config.fog_credentials = {
+      provider:              'AWS',                        # required
+      aws_access_key_id:     ENV["AWS_ACCESS_KEY"],        # required
+      aws_secret_access_key: ENV["AWS_SECRET_KEY"],        # required
+    }
+    config.fog_directory  = ENV["AWS_BUCKET"]              # required
+  end
 end
