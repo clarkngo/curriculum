479f2a9371124279d455d4a521b216e4005bd14c
Clark Ngo <clarkngo@gmail.com>
2019-02-19 21:28:06 -0800

    fix feature test for user adds a course

---
+++ spec/features/user_adds_course_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.feature "user adds a course process", type: :feature do
+  scenario 'user adds a course' do
+
+    # arrange
+    user = FactoryBot.create(:user)
+    category = FactoryBot.create(:category)
+
+    visit '/'
+    click_on 'Sign in'
+
+    fill_in 'Email', :with => user.email
+    fill_in 'Password', :with => user.password
+
+    click_on 'Log in'
+    
+    visit new_course_path
+
+    select("Preparatory", :from => "Category")
+    fill_in 'Name', :with => "Intro to ML"
+    fill_in 'Description', :with => "This course is an introduction to ML."
+
+    # act    
+    click_on 'Submit!'
+
+    # assert
+    expect(Course.last.name).to eq("Intro to ML")
+  end
+end
