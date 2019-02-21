e8182765c0d678d2d6fb59108292fc82e76e24c9
Clark Ngo <clarkngo@gmail.com>
2019-02-20 17:05:46 -0800

    add mount_uploader for photo_header

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,5 +1,6 @@
 class Course < ApplicationRecord
   mount_uploader :photo, PhotoUploader
+  mount_uploader :photo_header, PhotoUploader
 
   validates :category_id, presence: true
   validates :name, presence: true
