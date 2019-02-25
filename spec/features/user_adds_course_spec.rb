509086d1ace84fac5162140c7e01adbdfbeb1140
Clark Ngo <clarkngo@gmail.com>
2019-02-25 14:25:35 -0800

    demo

--- spec/features/user_adds_course_spec.rb
+++ spec/features/user_adds_course_spec.rb
@@ -7,7 +7,8 @@ RSpec.feature "user adds a course process", type: :feature do
     user = FactoryBot.create(:user)
     category = FactoryBot.create(:category)
 
-    visit '/users/sign_in'
+    visit '/'
+    click_on 'Sign in'
 
     fill_in 'Email', :with => user.email
     fill_in 'Password', :with => user.password
